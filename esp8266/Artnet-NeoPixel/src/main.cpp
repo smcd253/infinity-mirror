fc53402ed20beee894979a3606eb39a4e12fcf48
Spencer McDonough <shmcdono@usc.edu>
2020-08-11 18:04:35 -0700

    update

--- esp8266/Artnet-NeoPixel/src/main.cpp
+++ esp8266/Artnet-NeoPixel/src/main.cpp
@@ -5,16 +5,17 @@ Name: HyperCube   | IP ADDR: 192.168.1.102 | UNIVERSE(s): 3, 4 | NUM LEDS: 184 |
 Name: Mirrors1_2  | IP ADDR: 192.168.1.103 | UNIVERSE(s): 5    | NUM LEDS: 137 | DIMENSIONS: 17, 17, 17, 17, 15, 18, 17, 2 
 Name: Mirror3     | IP ADDR: 192.168.1.104 | UNIVERSE(s): 6    | NUM LEDS: 69  | DIMENSIONS: 17, 17, 17, 17, 15, 18, 17, 2 
 Name: Mirrors4_5  | IP ADDR: 192.168.1.105 | UNIVERSE(s): 7    | NUM LEDS: 136 | DIMENSIONS: 17, 17, 17, 17, 15, 17, 17, 2 
+Name: Mirrors4_5  | IP ADDR: 192.168.1.106 | UNIVERSE(s): 8,9  | NUM LEDS: 206 | DIMENSIONS: 17, 17, 18, 17, 17, 17, 18, 17, 17, 17, 17, 18 
 */
 #include <ArtnetWifi.h>
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define DEVICE_NAME "HyperCube"
-IPAddress ip(192,168,1,103);
-const int num_universes = 2;
-const int universes[num_universes] = {3, 4};
-const int leds_per_universe[num_universes] = {204};
+#define DEVICE_NAME "Mirrors1_2"
+IPAddress ip(192,168,1,136);
+const int num_universes = 1;
+const int universes[num_universes] = {5};
+const int leds_per_universe[num_universes] = {137};
 const int start_pixel[num_universes] = {0};   
 const int numLeds = 137;
 const int data_pin = 3;
