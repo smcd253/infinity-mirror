518821cceb8d4e947390efef2f37cbafe19c7fde
Spencer McDonough <shmcdono@usc.edu>
2020-09-04 07:59:51 -0700

    add web server for esp standalone

--- esp8266/Artnet-NeoPixel/src/main.cpp
+++ esp8266/Artnet-NeoPixel/src/main.cpp
@@ -5,19 +5,19 @@ Name: HyperCube   | IP ADDR: 192.168.1.102 | UNIVERSE(s): 3, 4 | NUM LEDS: 184 |
 Name: Mirrors1_2  | IP ADDR: 192.168.1.103 | UNIVERSE(s): 5    | NUM LEDS: 137 | DIMENSIONS: 17, 17, 17, 17, 15, 18, 17, 2 
 Name: Mirror3     | IP ADDR: 192.168.1.104 | UNIVERSE(s): 6    | NUM LEDS: 69  | DIMENSIONS: 17, 17, 17, 17, 15, 18, 17, 2 
 Name: Mirrors4_5  | IP ADDR: 192.168.1.105 | UNIVERSE(s): 7    | NUM LEDS: 136 | DIMENSIONS: 17, 17, 17, 17, 15, 17, 17, 2 
-Name: Mirrors4_5  | IP ADDR: 192.168.1.106 | UNIVERSE(s): 8,9  | NUM LEDS: 206 | DIMENSIONS: 17, 17, 18, 17, 17, 17, 18, 17, 17, 17, 17, 18 
+Name: Mirrors6_9  | IP ADDR: 192.168.1.106 | UNIVERSE(s): 8, 9 | NUM LEDS: 206 | DIMENSIONS: 17, 17, 18, 17, 17, 17, 18, 17, 17, 17, 17, 18 
 */
 #include <ArtnetWifi.h>
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define DEVICE_NAME "Mirrors1_2"
-IPAddress ip(192,168,1,136);
-const int num_universes = 1;
-const int universes[num_universes] = {5};
-const int leds_per_universe[num_universes] = {137};
-const int start_pixel[num_universes] = {0};   
-const int numLeds = 137;
+#define DEVICE_NAME "MirrorMatrix-col1"
+IPAddress ip(192,168,1,103);
+const int num_universes = 2;
+const int universes[num_universes] = {5, 6};
+const int leds_per_universe[num_universes] = {138, 69};
+const int start_pixel[num_universes] = {0, 138};   
+const int numLeds = 207;
 const int data_pin = 3;
 
 //Wifi settings
@@ -73,6 +73,7 @@ boolean ConnectWifi(void)
 
 void initTest()
 {
+  // Serial.println("init leds");
   int total_leds = 0;
   for (int i = 0; i < sizeof(leds_per_universe)/sizeof(leds_per_universe[0]); i++) {
     total_leds += leds_per_universe[i];
@@ -111,9 +112,9 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
         leds.setPixelColor(j, data[l], data[l + 1], data[l + 2]);
         k++;      
       }
+      leds.show();
     }
   }
-  leds.show();
 }
 
 void setup()
